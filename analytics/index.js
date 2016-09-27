93d3195b42c127a6a2663b4f431e442b09a91fbb
Kevin Swiber <kswiber@gmail.com>
2016-09-26 22:43:55 -0700

    Adding a config option to mask the request URI in analytics.

--- analytics/index.js
+++ analytics/index.js
@@ -2,12 +2,18 @@
 
 var debug = require('debug')('plugin:analytics');
 var volos = require('volos-analytics-apigee');
-module.exports.init = function(config, logger, stats) {
 
+var MASK = '**********';
+
+module.exports.init = function(config, logger, stats) {
   config.finalizeRecord = function finalizeRecord(req, res, record, cb) {
     if (res.proxy) {
       record.apiproxy = res.proxy.name;
       record.apiproxy_revision = res.proxy.revision;
+    }
+    if (config.maskUri === true) {
+      record.request_uri = MASK;
+      record.request_path = MASK;
     }
     cb(null, record);
   };
