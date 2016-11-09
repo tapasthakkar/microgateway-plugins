71478d84cd6088db35e5023732032b44f1cb20c3
Mitch Fierro <sendtofierro@gmail.com>
2016-11-09 14:26:32 -0800

    remove demo hack

--- verify-api-key/index.js
+++ verify-api-key/index.js
@@ -4,8 +4,8 @@ var util = require('util');
 module.exports.init = function(config, logger, stats) {
   
   var apidUrl = (process.env.APID_ENDPOINT || 'http://localhost:9090') + '/verifiers/apikey';
-  //TODO until apid is fully working, we need this override
-  apidUrl = 'http://localhost:9090/verifiers/apikey';
+  // TODO until apid is fully working, we need this override
+  // apidUrl = 'http://localhost:9090/verifiers/apikey';
   return {
     onrequest: function(req, res, data, next) {
       var scope = res.proxy.scope;
@@ -28,6 +28,7 @@ module.exports.init = function(config, logger, stats) {
             action: 'verify'
           }
         }
+        console.log("Options: -----\n\n", options);
         request.post(options, function (err, resp, body) {
           var jsonBody = JSON.parse(body);
           if (err) {
