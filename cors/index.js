d784499134aa8a046f3608cda70dbb34bad50375
Nandan Sridhar <srinandans@google.com>
2017-10-30 15:11:02 -0700

    support externalizing cors options

--- cors/index.js
+++ cors/index.js
@@ -1,16 +1,20 @@
 'use strict'
-var debug = require('debug')('plugin:corsfix');
+var debug = require('debug')('plugin:cors');
 
 module.exports.init = function(config, logger, stats) {
+
+  var methods = config['cors-methods'] || 'GET, PUT, POST, DELETE, PATCH, OPTIONS';
+  var maxAge = config['cors-max-age'] || '3628800';
+  var allowHeaders = config['cors-headers'] || 'Origin, x-requested-wuth, Accept, Content-Type, Accept-Encoding, Accept-Language, Host, Pragma, Referrer, User-Agent, Cache-Control, Authorization, x-api-key';
+
   return {
   	onrequest: function(req, res, next) {
 	  if(req.method == 'OPTIONS') {
 	    debug('settings cors headers');
 	    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
-	    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
-       	    res.setHeader('Access-Control-Allow-Max-Age', '3628800');
-	    //some browsers do not allow Authorization in the allow-headers response.
-	    res.setHeader('Access-Control-Allow-Headers', 'Origin, x-requested-with, Accept, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Pragma, Referrer, User-Agent, Cache-Control, x-api-key, Authorization, X-NLSN-Authorization');
+	    res.setHeader('Access-Control-Allow-Methods', methods);
+       	    res.setHeader('Access-Control-Allow-Max-Age', maxAge);
+	    res.setHeader('Access-Control-Allow-Headers', allowHeaders);
 	    res.end();
 	  } else {
 	    debug('skipping cors plugin');
