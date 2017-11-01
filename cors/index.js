28e8ead637d9c08141fa4d5d1f282f52d3afea86
Nandan Sridhar <srinandans@google.com>
2017-11-01 10:04:57 -0700

    support externalizing cors origin

--- cors/index.js
+++ cors/index.js
@@ -6,12 +6,14 @@ module.exports.init = function(config, logger, stats) {
   var methods = config['cors-methods'] || 'GET, PUT, POST, DELETE, PATCH, OPTIONS';
   var maxAge = config['cors-max-age'] || '3628800';
   var allowHeaders = config['cors-headers'] || 'Origin, x-requested-wuth, Accept, Content-Type, Accept-Encoding, Accept-Language, Host, Pragma, Referrer, User-Agent, Cache-Control, Authorization, x-api-key';
+  var origin = config['cors-origin'];
 
   return {
   	onrequest: function(req, res, next) {
 	  if(req.method == 'OPTIONS') {
 	    debug('settings cors headers');
-	    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
+	    if (origin) res.setHeader('Access-Control-Allow-Origin', origin); 
+            else res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
 	    res.setHeader('Access-Control-Allow-Methods', methods);
        	    res.setHeader('Access-Control-Allow-Max-Age', maxAge);
 	    res.setHeader('Access-Control-Allow-Headers', allowHeaders);
