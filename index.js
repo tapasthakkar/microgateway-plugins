68a3843677e748b3dab091c554eef4d371d3f115
relloller <victorshaw@google.com>
2019-04-30 10:13:47 -0400

    add ntlm

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ module.exports = {
   apikeys: require('./apikeys'),
   oauthv2: require('./oauthv2'),
   extauth: require('./extauth'),
-  invalidHeader: require('./invalidHeader')
+  invalidHeader: require('./invalidHeader'),
+  ntlm: require('./ntlm')
 }
