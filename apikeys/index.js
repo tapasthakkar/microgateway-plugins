77530a3e6c6d0e1a4c2e85b28e0f6e5032dd070c
Nandan Sridhar <srinandans@google.com>
2019-05-17 14:36:07 -0700

    fix for b132970272

--- apikeys/index.js
+++ apikeys/index.js
@@ -66,7 +66,7 @@ module.exports.init = function(config, logger, stats) {
     }
 
     var exchangeApiKeyForToken = function(req, res, next, config, logger, stats, middleware, apiKey) {
-        var cacheControl = req.headers["cache-control"] || 'no-control';
+        var cacheControl = req.headers["cache-control"] || 'no-cache';
         if (cacheKey || (cacheControl && cacheControl.indexOf("no-cache") < 0)) { // caching is allowed
             cache.read(apiKey, function(err, value) {
                 if (value) {
@@ -193,7 +193,7 @@ module.exports.init = function(config, logger, stats) {
             req.headers["x-authorization-claims"] = new Buffer(JSON.stringify(authClaims)).toString("base64");
 
             if (apiKey) {
-                var cacheControl = req.headers["cache-control"];
+                var cacheControl = req.headers["cache-control"] || "no-cache";
                 if (cacheKey || (cacheControl && cacheControl.indexOf("no-cache") < 0)) { // caching is toFixed
                     // default to now (in seconds) + 30m if not set
                     decodedToken.exp = decodedToken.exp || +(((Date.now() / 1000) + 1800).toFixed(0));
